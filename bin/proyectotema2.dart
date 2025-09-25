098b12a9b80785c5532ce0f64af093b7600b4465
= <adrianramos2017@outlook.com>
2025-09-25 11:05:33 +0200

    proyecto comenzado

---
+++ bin/proyectotema2.dart
@@ -0,0 +1,15 @@
+import 'package:proyectotema2/proyectotema2.dart' as proyectotema2;
+import 'dart:io';
+
+void mostrarMenu(){
+  stdout.write("Tienda Iphone");
+  stdout.write("1.- Añadir articulo");
+  stdout.write("2.- Modificar articulo");
+  stdout.write("3.- Eliminar articulo");
+  stdout.write("4.- Listar articulos");
+  stdout.write("5.- Buscar por elemento");
+  stdout.write("6.- Salir");
+}
+void main(List<String> arguments) {
+  
+}
---
+++ test/proyectotema2_test.dart
@@ -0,0 +1,8 @@
+import 'package:proyectotema2/proyectotema2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
